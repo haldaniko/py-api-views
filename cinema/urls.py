dc7242bffdb9f500f07e6b2ec79cfe768bae93f5
haldaniko <haldaniko@gmail.com>
2024-06-29 00:19:27 +0200

    add all urls for views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,45 @@
 from django.urls import path
+from cinema.views import (
+    ActorList,
+    ActorDetail,
+    GenreList,
+    GenreDetail,
+    CinemaHallList,
+    CinemaHallViewSet,
+    MovieViewSet
+)
 
-from cinema.views import movie_list, movie_detail
+app_name = "cinema"
+
+movie_list = MovieViewSet.as_view(actions={
+    "get": "list",
+    "post": "create"
+})
+
+movie_detail = MovieViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"
+})
+
 
 urlpatterns = [
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", CinemaHallList.as_view(actions={
+        "get": "list",
+        "post": "create"
+    }), name="cinemahall-list"),
+    path("cinema_halls/<int:pk>/", CinemaHallViewSet.as_view(actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+    ), name="actor-detail"),
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
-
-app_name = "cinema"
